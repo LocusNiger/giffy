3ff615a481017674f39ef320bdfadabc2da0e956
Iván Sánchez <sanchezivan.798@gmail.com>
2022-05-17 15:19:01 -0300

    V1. gif search (alpha)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+
+const apiUrl =
+  "https://api.giphy.com/v1/gifs/search?api_key=XFqiWoyn4nHms0AxsCQuaepcc2VnKztd&q=LeBron&limit=10&offset=0&rating=g&lang=en";
 
 function App() {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(function () {
+    console.log("Actualizando gifs");
+    fetch(apiUrl) /* Realizo el fetch de la API. Devuelve una promesa => */
+      .then((res) => res.json()) /* Transformo la rta. en JSON */
+      .then((response) => {
+        /* Accedemos al json para mostrar las imagenes. Nos devuelve DATA */
+        const { data } = response; /* Data es un array de objetos, y cada obj. es un GIF */
+        const gifs = data.map(
+          (image) => image.images.downsized_medium.url
+        ); /* En gifs tengo un array de gifs, lo recorro con map y extraigo de c/gif su url */
+        setGifs(gifs); /* Seteo a gifs como un array de url's */
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <h1>Hello Bron</h1>
+        {/* Recorro el array gifs */}
+        {gifs.map((singleGif) => (
+          <img src={singleGif} alt="a gif" />
+        ))}
+      </section>
     </div>
   );
 }
