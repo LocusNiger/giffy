d2775eea28f54e1d01bc5864a94eaa394364ef4d
Iván Sánchez <sanchezivan.798@gmail.com>
2022-06-23 11:22:23 -0300

    Little changes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
 import { Route } from "wouter";
 import "tailwindcss/tailwind.css";
-import LastSearch from "./components/LastSearch";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="min-h-screen bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
-      <section className="App-content flex flex-col items-center min-h-screen">
+      <section className=" flex flex-col items-center min-h-screen">
         <Logo />
         {/* route: cuando el path sea X, el componente a renderizar es Y */}
         <Route path="/" component={Home} />
-        <Route path="/" component={LastSearch} />
         <Route path="/search/:keyword" component={Home} />
         <Route path="/search/:keyword" component={SearchResults} />
       </section>
